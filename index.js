c73bb93ade2c8739c591cd9d5359c30c3b597a86
mahubane <mahubanemk@gmail.com>
2023-02-26 22:28:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+window.onload = function()  {
+const DATE = new Date();
+    
+let  name  ="<b >Gift </b>";
+let  surname = "<b>Mahubane</b>" ;
+let  bio = "<b>I am Gift from polokwane.i reflect a varied  personality including ambition </b>" ;
+let  educationalBackground = " <b>Ndip software development </b>";
+ let  contactNumber   = '<b>0766619382<b> ';
+
+  
+
+ 
+let text = "<h1>" + DATE+"</h1>";
+document.body.innerHTML="<h1>  This is my personal information:</h1>   "+
+name+ "," + surname +" , " + bio + "," +   educationalBackground +" , "+ contactNumber;
+
+ 
+console.log("Hello Console",DATE) 
+
+};
+
+
+    //let project = new Array(
+      // {img:' ./Assets/images/a2dc3249364449a49f01a6275d277b8c.jpg.',item: "c++" ,level: 8, },
+      //  {img:'  ./Assets/images/cisco.png',item: "cisco",level: 6},
+     //  {img:'  ./Assets/images/R.png',item: "java",level: 5} 
+     //);
+     
+     //   let listUl = "<div>";
+      //  for (let i= 0; i < project.length; i++) 
+      //  {
+     //  console.log(project[i].item);
+     // listUl += "<table border ='1'><tr><td rowspan='3'style='width: 10%'> <img src='"+ project[i] .img+"' alt='' style='width: 100px;height: 100px  ;border-radius: 50%'/>" + 
+      //  "</td></tr><tr> <td>"+project[i].item+ "</td></tr><tr><td> "+project[i].level+"</td></tr></table>";
+      //  }
+       /* <table> 
+    //<tr>
+       // <td rowspan ="2">< img src="" alt="" style="width: 100px;height: 100px  ;border-radius: 50px" /></td>
+    //<td>
+         //   project
+       // </td>
+    //</tr>
+    //<tr>
+      //  <td>
+       //     level
+       // </td>
+   // </tr>
+</table>*/
+       
+    
+    
+    
+
+    
+
